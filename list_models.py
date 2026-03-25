5d4526f2d40ce4fec6c841a0b65eafd9f7db2ad0
Manish_KD <92296330+ManishKDtm@users.noreply.github.com>
2026-03-26 01:47:09 +0530

    Delete list_models.py

--- list_models.py
+++
@@ -1,8 +0,0 @@
-from google import genai
-
-client = genai.Client(api_key="AIzaSyDmssGGvD2Ur5AFfgZAR8ZHSOUtE_8YUgU")
-
-models = client.models.list()
-
-for model in models:
-    print(model.name, model.supported_actions)
